acff2bba2e99eeb03c6df09f3c7987994efd951e
Lisa Holland <lholland@interthinx.com>
2015-02-19 23:13:40 -0600

    Prog Assgn 2 LMH

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## calculate the inverse of a matrix, cache the solution, and recall it if needed again 
+## without having to recalucate it every time it is asked for in code
 
-## Write a short comment describing this function
+## creates a matrix to both set and get the inverse of a matrix when needed 
+makeCacheMatrix <- function(x = matrix()) { 
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  ## sets the value of vector
+  
+  get <- function() x
+  ## gets the value of the vector
+  
+  setinv <- function(solve) m <<- solve
+  ## sets the value of the inverse of the matrix
+  
+  getinv <- function() m
+  ## gets the value of the inverse of the matrix
+  
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+} 
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## creates the inverse of a matrix or obtains it from the cached data 
+cacheSolve <- function(x, ...) { 
+  ## Return a matrix that is the inverse of 'x' 
+  m <- x$getinv()
+  ## checks to see if the inverse has already been cached
+  
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  ## if the inverse has already been cached (is NOT NULL) a message "getting cached data" is displayed
+  ## while that information is being extracted
+  
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
+} 
